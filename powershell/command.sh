1860213240ffe2aec5aa703daf74439fd9528913
classintown <37047311+classintown@users.noreply.github.com>
2025-10-06 21:00:42 +0530

    Update command.sh

--- powershell/command.sh
+++ powershell/command.sh
@@ -1,3 +1,8 @@
 powershell -NoProfile -ExecutionPolicy Bypass -File C:\CIT-ping\cit_monitor.ps1
 Get-Content C:\CIT-ping\cit_ping.log -Tail 3
 if (Test-Path C:\CIT-ping\cit_ping.csv) { Import-Csv C:\CIT-ping\cit_ping.csv | Select-Object -Last 3 }
+
+
+# To check
+Get-ScheduledTask -TaskName "CIT Monitor (15m)" | Get-ScheduledTaskInfo |
+  Select-Object LastRunTime, NextRunTime, LastTaskResult
